9fc5b5745dcbaa6828e9a678261ffbb77b26cf7b
Romain <romainjacquel3@gmail.com>
2020-02-26 19:39:34 +0100

    feat: contact form

--- front/src/components/form-contact/index.jsx
+++ front/src/components/form-contact/index.jsx
@@ -44,78 +44,36 @@ export default function FormContact() {
 
 
         return (
-            <div id="contact" className={"container-fluid position-relative h-100"}>
-                <div className={"particles-js"}> </div>
-                <div className={"row p-md-5 mx-5"}>
-                    <div className={"col-md-8 p-md-5"}>
-                        <h1 className={"call-us py-5 text-left"} data-aos="fade-up">
-                            APPELEZ NOUS !
-                        </h1>
-                        <div
-                            className={"card p-5"}
-                            id="call-box"
-                            data-aos="fade-up"
-                            data-aos-duration="1000"
-                        >
-                            <form id={"form-contact"} method="POST">
-                                <div className={"form-row"}>
-                                    <div className={"col-md-6"}>
-                                        <div className={"form-group"}>
-                                            <label htmlFor={"name-label"}>Votre nom</label>
-                                            <input
-                                                id="name"
-                                                type="text"
-                                                className={"form-control form-control-lg"}
-                                                placeholder="Quel est votre nom ?"
-                                                value={name}
-                                                name='name'
-                                                onChange={e => updateFormData(e)}
-                                            />
-                                        </div>
-                                    </div>
-                                    <div className={"col-md-6"}>
-                                        <div className={"form-group"}>
-                                            <label htmlFor={"email-label"}>Email</label>
-                                            <input
-                                                id="email"
-                                                type="text"
-                                                className={"form-control form-control-lg"}
-                                                placeholder="Quel est votre adresse mail ?"
-                                                value={email}
-                                                onChange={e => updateFormData(e)}
-                                                name='email'
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
+            <div id="contact" className="formContactGlobal">
 
-                                <div className={"form-row"}>
-                                    <div className={"col-md-12"}>
-                                        <div className={"form-group"}>
-                                            <label htmlFor={'message-label'}>Message</label>
-                                            <textarea
-                                                className={"form-control form-control-lg"}
-                                                id="message"
-                                                rows="3"
-                                                placeholder="Your message"
-                                                value={message}
-                                                onChange={e => updateFormData(e)}
-                                                name='message'
-                                            ></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                                <input type='submit' onClick={onSubmit} value='Valider' />
-                                <input
-                                    style={{ display: "block", marginTop: 10 }}
-                                    type="reset"
-                                    value="Reset"
-                                />
-                            </form>
-                        </div>
+                    <p>APPELEZ-NOUS</p>
+
+
+                <div className="formContact">
+                    <div className="formFirstPart">
+                        <label>Nom :</label>
+                        <input className="formContactInput" type="text" name="lastName"/>
+
+                        <label>Code postale :</label>
+                        <input className="formContactInput" type="text" name="zipCode"/>
+
+                        <label>Email :</label>
+                        <input className="formContactInput" type="text" name="email"/>
+
+                        <label>Téléphone :</label>
+                        <input className="formContactInput" type="text" name="phoneNumber"/>
+
+                        <label>Date(s) de l'évènement :</label>
+                        <input className="formContactInput" type="text" name="eventDate"/>
                     </div>
-                    <div className={"col-md-4 "}></div>
+                    <div className="formSecondPart">
+                        <label>Message :</label>
+                        <input className="formContactInput" type="textarea" name="message"/>
+                        <button type='submit'>ENVOYER</button>
+
+                    </div>
                 </div>
+                <img src="assets/img/perso_ligne_femme.png" alt='perso_vr' />
             </div>
         )
 }
